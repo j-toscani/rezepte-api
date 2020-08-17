59a0905bb1a3df4c3627d5d5a0d1ecc4143b6444
j-toscani <julian_toscani@gmx.de>
2020-08-17 22:33:23 +0200

    add env vars

--- index.ts
+++ index.ts
@@ -4,23 +4,25 @@ import cors from "cors";
 
 export const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.disable("x-powered-by");
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.get("/data", (req, res) => {
+app.get("/", (req, res) => {
   res.send({ message: "Hello" });
 });
 
-app.post("/data", (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
   res.send({ mesage: "Ok" });
 });
 
 export const start = () => {
-  app.listen(3000, () => {
-    console.log("Server is on 3000");
+  app.listen(port, () => {
+    console.log(`Server listening on ${port}`);
   });
 };
