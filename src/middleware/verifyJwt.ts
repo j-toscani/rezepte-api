c22525cd2a8f0a142a07aec066b63c7e42ee7d9c
Julian Toscani <julian_toscani@gmx.de>
2021-02-26 16:20:18 +0100

    unify use of res and req

--- src/middleware/verifyJwt.ts
+++ src/middleware/verifyJwt.ts
@@ -2,26 +2,26 @@ import { Response, Request, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export default function verifyJwt(
-  request: Request,
-  response: Response,
+  req: Request,
+  res: Response,
   next: NextFunction
 ) {
-  const authHeader = request.headers["authorization"];
+  const authHeader = req.headers["authorization"];
 
   if (!authHeader) {
-    response.status(401).send("Auth Header missing.");
+    res.status(401).send("Auth Header missing.");
     return;
   }
 
   const token = authHeader.split(" ")[1];
-  if (token == null) return response.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
 
   if (!process.env.SECRET) {
     throw new Error("NO SECRET PROVIDED");
   }
 
   jwt.verify(token, process.env.SECRET, (error, _user) => {
-    if (error) return response.sendStatus(403);
+    if (error) return res.sendStatus(403);
     next();
   });
 }
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -6,8 +6,8 @@ import { findUser, createUser } from "../db/actions/user";
 
 const router = Router();
 
-router.post("/register", async (request, response) => {
-  const { name, password } = request.body;
+router.post("/register", async (req, res) => {
+  const { name, password } = req.body;
   const hasPasswordAndName =
     name &&
     password &&
@@ -15,45 +15,41 @@ router.post("/register", async (request, response) => {
     typeof password === "string";
 
   if (!hasPasswordAndName) {
-    response.status(400).send("No sufficient data.");
+    res.status(400).send("No sufficient data.");
     return;
   }
   try {
     const user = await createUser({ name, password });
 
     if (user) {
-      response.status(200).send(`User ${user.name}, created!`);
+      res.status(200).send(`User ${user.name}, created!`);
     }
   } catch (err) {
-    response.status(400).send(err);
+    res.status(400).send(err);
     return;
   }
 });
 
-router.post("/login", async (request, response) => {
-  const { name, password } = request.body;
-  const hasPasswordAndName =
-    name &&
-    password &&
-    typeof name === "string" &&
-    typeof password === "string";
+router.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  const hasPasswordAndName = name && password;
 
   if (!hasPasswordAndName) {
-    response.status(400).send("No sufficient data.");
+    res.status(400).send("No sufficient data.");
     return;
   }
   try {
     const user = await findUser({ name, password });
 
     if (!user) {
-      response.status(404).send("User not found.");
+      res.status(404).send("User not found.");
       return;
     }
 
     const verified = verify(password, user.password);
 
     if (!verified) {
-      response.status(401).send("Wrong password or Username.");
+      res.status(401).send("Wrong password or Username.");
     }
 
     if (!process.env.SECRET) {
@@ -62,11 +58,15 @@ router.post("/login", async (request, response) => {
 
     const token = jwt.sign(name, process.env.SECRET);
 
-    response.status(200).send(token);
+    res.status(200).send({ token });
   } catch (err) {
-    response.status(400).send(err);
+    res.status(400).send(err);
     return;
   }
+});
+
+router.post("/logout", (req, res) => {
+  res.status(200).send({ message: "You are logged out, goodbye!" });
 });
 
 export default router;
